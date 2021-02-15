e7f91f88e40191488c6b9cfe934e9e672866fa2d
Pere Esteve <hitaboy@gmail.com>
2021-02-15 17:23:08 +0100

    Router and first lines of experiments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch, 
+  Route,
+  Link
+} from 'react-router-dom'
+import Experiment_1 from './experiments/e_1.js'
+import Experiment_2 from './experiments/e_2.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <>
+        <h1>React Three Playground</h1>
+        <>
+          <>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/Experiment_1">Exp_1</Link>
+              </li>
+              <li>
+                <Link to="/Experiment_2">Exp_2</Link>
+
+              </li>
+            </ul>
+          </>
+          <>
+            <Switch>
+              <Route path="/experiment_1">
+                <Experiment_1 />
+              </Route>
+              <Route path="/experiment_2">
+                <Experiment_2 />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </>
+        </>
+      </>
+    </Router>
+  )
+}
+
+function Home() {
+  return <h2>Home</h2>;
 }
 
 export default App;
---
+++ src/experiments/e_2.js
@@ -0,0 +1,3 @@
+export default function Experiment_2() {
+    return <h2>Exp_2</h2>;
+  }
